6e8d2113c172c0d08fa10eec3b0c69462471faae
JayaaJain <jayajn98@gmail.com>
2020-05-16 16:13:53 +0000

    ME_QBOX_MODULE_SERVER_CONFIGURATION

--- server_configuration/check_QBox_server_OS.sh
+++ server_configuration/check_QBox_server_OS.sh
@@ -18,7 +18,7 @@ requiredUbuntuVersion=18       # Ubuntu verison 18.0
     # Tasks:
     # 1) Add the line to populate the variable "linuxType" with the OS type.
     # 2) Start the expression to populate the variable with "cat $1" which is the command line filename input. Check the Usage above. 
-linuxType=$(hostnamectl | grep 'Operating System' | awk '{print $3}')
+linuxType=$(lsb_release -d | awk '{print $2}')
 if [ "$linuxType" = "$requiredOS1" ] || [ "$linuxType" = "$requiredOS2" ]
    then
    echo "OS is $requiredOS2 which is required for QBox"
@@ -34,8 +34,10 @@ fi
     # 2) Start the expression to populate the variable with "cat $1" which is the command line filename input. Check the Usage above. 
     # 3) Bash script can only do integer arithmetic. Include additional handling needed to strip the decimal part
     # 4) Execute the script before submission and confirm the output 
-x=$(hostnamectl | grep 'Operating System' | awk '{print $4}')
+
+x=$(lsb_release -d | awk '{print $3}')
 ubuntuVersion=${x%.*.*}
+
 if test $ubuntuVersion -ge $requiredUbuntuVersion
    then
    echo "Ubuntu version is $ubuntuVersion which is sufficient for QBox"
